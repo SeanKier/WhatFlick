4f6e9a59341d54080f504c60399b167d7059869e
SeanKier <seantkier@gmail.com>
2020-03-29 13:25:13 -0700

    adds rendering of movie poster images

--- client/src/Movies.js
+++ client/src/Movies.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CurrentMovie = ({ currentMovie }) => (
-  <div id="single-movie-container">
-    <h2>
-      {currentMovie.title}
-    </h2>
-    <h4>
-      {currentMovie.vote_average}
-    </h4>
-    <p>
-      {currentMovie.overview}
-    </p>
-  </div>
-);
+const CurrentMovie = ({ currentMovie }) => {
+  const { title, poster_path, vote_average, overview } = currentMovie;
+
+  return (
+    <div id="single-movie-container">
+      <h2>
+        {title}
+      </h2>
+      <img
+        className='avatar'
+        src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+        alt={`Avatar for ${title}`}
+      />
+      <h4>
+        {vote_average}
+      </h4>
+      <p>
+        {overview}
+      </p>
+    </div>
+  );
+};
 
 CurrentMovie.propTypes = {
   currentMovie: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    vote_average: PropTypes.string.isRequired,
+    vote_average: PropTypes.number.isRequired,
     overview: PropTypes.string.isRequired
   })
 };
