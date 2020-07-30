d6ba5ccb22918a2c79d64c29d4de294a6b77aea2
SeanKier <seantkier@gmail.com>
2020-07-29 19:40:57 -0700

    broken nonsense

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,8 +7,10 @@ import api_key from './APIKEY';
 const App = (props) => {
   const [genre, changeGenre] = useState('Now Playing');
   const [movies, updateMovies] = useState([]);
+  const [page, nextPage] = useState(1);
 
   const updateNewGenre = (event) => {
+
     const newGenre = event.target.innerHTML;
     if (newGenre !== genre) {
       getData(newGenre);
@@ -16,26 +18,52 @@ const App = (props) => {
   }
   useEffect(() => {
     getData(genre);
-  }, []);
+  }, [page]);
 
-  const getData = (genre) => {
+  const handleNextPage = () => {
+    nextPage(page + 1);
+  }
+
+  const getData = (newGenre) => {
+
     const options = {
       'Now Playing': 'now_playing',
       'Popular': 'popular',
       'Top Rated': 'top_rated',
       'Upcoming': 'upcoming'
     }
-    fetch(`https://api.themoviedb.org/3/movie/${options[genre]}?api_key=${api_key}&language=en-US&page=1`)
+
+    if (newGenre === genre) {
+      fetch(`https://api.themoviedb.org/3/movie/${options[newGenre]}?api_key=${api_key}&language=en-US&page=${page}`)
       .then(response => {
         return response.json();
       })
       .then(response => {
+        changeGenre(newGenre);
+        const items = movies.slice();
+        response.results.forEach((item)=> {
+          items.push(item);
+        })
+        updateMovies(items);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    } else {
+      fetch(`https://api.themoviedb.org/3/movie/${options[genre]}?api_key=${api_key}&language=en-US&page=1`)
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        console.log('hellofdjfpkdfjdslkfjdslk;g', response.results)
         changeGenre(genre);
         updateMovies(response.results);
       })
       .catch(err => {
         console.log(err);
       });
+    }
+
   }
 
   const genres = ['Now Playing', 'Popular', 'Top Rated', 'Upcoming'];
@@ -45,6 +73,11 @@ const App = (props) => {
       <h1 id="site-title" className="center">Watch Tonight</h1>
       <Navbar options={genres} currentGenre={genre} changeGenre={updateNewGenre} />
       <Movies currentMovies={movies}/>
+      <button
+        onClick={handleNextPage}
+      >
+        .... More Movies ....
+      </button>
     </div>
   );
 }
