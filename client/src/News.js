162cfe65dc3390c011418a96b47e99403ce72ac8
SeanKier <seantkier@gmail.com>
2020-08-11 14:24:07 -0700

    adds news components

--- client/src/News.js
+++ client/src/News.js
@@ -1,33 +1,75 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-const news = () => {
+const StoryItem = ({ story }) => {
+
+  const { title, urlToImage, description } = story;
+
+  return (
+    <li>
+      <h4>
+        {title}
+      </h4>
+      <img
+        className="other-rounded"
+        src={urlToImage}
+        alt={`Backdrop for ${title}`}
+      />
+        <p>
+        {description}
+      </p>
+    </li>
+  );
+};
+
+StoryItem.propTypes = {
+  story: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    urlToImage: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
+  })
+};
+
+const News = () => {
+  const [netflixNews, updateNeflixNews] = useState([]);
 
   useEffect(() => {
     getNews();
   }, []);
 
   const getNews = () => {
-    fetch('v2/top-headlines?country=us&apiKey=b3b7eadbaf734646a234c44b765d31f3')
+    fetch('v2/everything?q=new netflix&sortBy=relevancy&apiKey=b3b7eadbaf734646a234c44b765d31f3')
       .then(response => {
         return response.json();
       })
       .then(response => {
-        console.log('news>>>>>>>>>', response);
+        const sorted = response.articles.sort((a, b) => {
+          b.publishedAt - a.publishedAt;
+        })
+        updateNeflixNews(sorted.slice(0, 4));
       })
       .catch(err => {
         console.log(err);
       });
   }
+
   return (
     <div className="news">
       <h3>
         News
       </h3>
+        <div>
+          <ul>
+            {netflixNews.map((story, i) => (
+              <StoryItem key={i} story={story} />
+            ))}
+          </ul>
+        </div>
+
     </div>
   );
 }
 
 
 
-export default news;
+export default News;
