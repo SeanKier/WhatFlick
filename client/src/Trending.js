de5203bad594e5da26e837e5643891e23c44035d
SeanKier <seantkier@gmail.com>
2020-08-18 11:25:24 -0700

    refactors Trending into seperate components

--- client/src/Trending.js
+++ client/src/Trending.js
@@ -4,24 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowAltCircleRight, faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
-const Trending = ({ movies, updateID }) => {
-  const [currentIndex, changeMovie] = useState(0);
-
-  const handleRightClick = () => {
-      if (currentIndex < 3) {
-        changeMovie(currentIndex + 1);
-      }
-
-  }
-  const handleLeftClick = () => {
-    if (currentIndex > 0) {
-      changeMovie(currentIndex - 1);
-    }
-  }
-  const whatsNext = movies.slice(currentIndex + 1);
-
-  return (
-    <div className="trending-carousel">
+const Carosouel = ({ movies, handleLeftClick, handleRightClick, currentIndex }) => (
+  <div className="carousel">
       { currentIndex > 0 && (
             <FontAwesomeIcon
               icon={faArrowAltCircleLeft}
@@ -34,23 +18,10 @@ const Trending = ({ movies, updateID }) => {
               onClick={handleRightClick}
             />
         )}
-        <h3>
-            Currently Trending
-        </h3>
-
-
          <div className={`cards-slider active-slide-${currentIndex}`}>
            <div className="cards-slider-wrapper" style={{
                   'transform': `translateX(-${currentIndex*(100/movies.length)}%)`
                 }}>
-             {/* <div className="card">
-                <img
-                    className="other-rounded card"
-                    src={`https://image.tmdb.org/t/p/w500/${movies[currentIndex].backdrop_path}`}
-                    alt={`Backdrop for ${movies[currentIndex].title}`}
-                />
-              </div> */}
-
             {movies.map((movie, i) => (
                   <div
                     id={`card-${i}`}
@@ -78,44 +49,80 @@ const Trending = ({ movies, updateID }) => {
 
            </div>
          </div>
+  </div>
+);
 
+Carosouel.propTypes = {
+  movies: PropTypes.array.isRequired,
+  handleLeftClick: PropTypes.func.isRequired,
+  handleRightClick: PropTypes.func.isRequired,
+  currentIndex: PropTypes.number.isRequired
+};
 
-        <div className="whats-next">
-          <h4>
-            Coming up Next
-          </h4>
-          <ul className={"trending-list"}>
-            {whatsNext.map((movie, i) => (
-              <div className="trending-item-container">
-                <li key={i}
-                className="trending-item"
-                >
-                  <div
-                    onClick={() => changeMovie(i + 1)}
-                  >
-                    <div className="trending-item-title">
-                    <Link onClick={() => updateID(movie.id)} to='/other'>
-                      <h1>
-                        {movie.title}
-                      </h1>
-                    </Link>
-                    </div>
-                    <Link onClick={() => updateID(movie.id)} to='/other'>
-                      <img
-                          className="trending-item-img"
-                          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                          alt={`Backdrop for ${movie.title}`}
-                      />
-                    </Link>
-                  </div>
-                </li>
+const ComingNext = ({ whatsNext }) => (
+  <div className="whats-next">
+    <h4>
+      Coming up Next
+    </h4>
+    <ul className={"trending-list"}>
+      {whatsNext.map((movie, i) => (
+        <div className="trending-item-container">
+          <li key={i}
+          className="trending-item"
+          >
+            <div
+              onClick={() => changeMovie(i + 1)}
+            >
+              <div className="trending-item-title">
+              <Link onClick={() => updateID(movie.id)} to='/other'>
+                <h1>
+                  {movie.title}
+                </h1>
+              </Link>
               </div>
-            ))}
-          </ul>
+              <Link onClick={() => updateID(movie.id)} to='/other'>
+                <img
+                    className="trending-item-img"
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={`Backdrop for ${movie.title}`}
+                />
+              </Link>
+            </div>
+          </li>
         </div>
+      ))}
+    </ul>
+  </div>
+);
 
+ComingNext.propTypes = {
+  whatsNext: PropTypes.array.isRequired
+};
+
+const Trending = ({ movies, updateID }) => {
+  const [currentIndex, changeMovie] = useState(0);
+
+  const handleRightClick = () => {
+      if (currentIndex < 3) {
+        changeMovie(currentIndex + 1);
+      }
+
+  }
+  const handleLeftClick = () => {
+    if (currentIndex > 0) {
+      changeMovie(currentIndex - 1);
+    }
+  }
+  const whatsNext = movies.slice(currentIndex + 1);
+
+  return (
+    <div className="trending-carousel">
+      <h2 className="title">
+        Currently Trending
+      </h2>
+      <Carosouel movies={movies} handleLeftClick={handleLeftClick} handleRightClick={handleRightClick} currentIndex={currentIndex} />
+      <ComingNext whatsNext={whatsNext} />
     </div>
-
 
   );
 };
