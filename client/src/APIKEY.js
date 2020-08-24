8ec31925e3d4dd784d3ea36fa4f3d7cb3647c8c5
SeanKier <seantkier@gmail.com>
2020-08-24 16:03:59 -0700

    update for aws

---
+++ client/src/APIKEY.js
@@ -0,0 +1,7 @@
+const api_key = 'd4cd9cca18498695e4dd01831aa44a15';
+const youtubeAPIkey = 'AIzaSyBzrdWIOJSQBjHOkG25qKt-8cfUbeK3Zws';
+const newsApi_key = 'b3b7eadbaf734646a234c44b765d31f3';
+
+module.exports.api_key = api_key;
+module.exports.youtubeAPIkey = youtubeAPIkey;
+module.exports.newsApi_key = newsApi_key;
